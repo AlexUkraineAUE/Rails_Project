f5f10b8d67d0675c1cb0f700a7390c60fe4985ad
AlexUkraineAUE <krosbond@gmail.com>
2024-03-06 00:06:28 -0600

    Add Genres Index and Show, Updated Games Show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+  get 'genres/show'
   get 'about', to: 'about#index'
   get 'games', to: 'games#index'
   get 'teams', to: 'teams#index'
@@ -7,6 +9,7 @@ Rails.application.routes.draw do
   resources :games, only: [:index, :show]
   resources :reviews, only: [:index, :show]
   resources :teams, only: [:index, :show]
+  resources :genres, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,53 @@
 require 'csv'
+require 'faker'
 
 Game.delete_all
 Genre.delete_all
 Team.delete_all
 Review.delete_all
+
+# # Number of fake games to generate
+# NUM_FAKE_GAMES = 20
+
+# # Create fake games
+# NUM_FAKE_GAMES.times do
+#   title = Faker::Game.title
+#   release_date = Faker::Date.between(from: 5.years.ago, to: Date.today)
+#   rating = Faker::Number.decimal(l_digits: 1, r_digits: 1)
+#   summary = Faker::Lorem.paragraph
+
+#   game = Game.create(
+#     title: title,
+#     release_date: release_date,
+#     rating: rating,
+#     summary: summary
+#   )
+
+#   # Create fake reviews for each game
+#   rand(1..5).times do
+#     review_content = Faker::Lorem.paragraph
+#     game.reviews.create(content: review_content)
+#   end
+
+#   # Create fake teams for each game
+#   rand(1..3).times do
+#     team_name = Faker::Team.name
+#     team = Team.find_or_create_by(name: team_name)
+#     game.teams << team
+#   end
+
+#   # Create fake genres for each game
+#   rand(1..3).times do
+#     genre_name = Faker::Game.genre
+#     genre = Genre.find_or_create_by(name: genre_name)
+#     game.genres << genre
+#   end
+# end
+
+# puts "Created #{Game.count} Games using Faker"
+# puts "Created #{Review.count} Reviews using Faker"
+# puts "Created #{Team.count} Teams using Faker"
+# puts "Created #{Genre.count} Genres using Faker"
 
 #fetch the filename
 filename = Rails.root.join("db/games.csv")
